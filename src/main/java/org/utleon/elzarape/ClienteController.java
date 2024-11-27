6d1a827bc0bd44a42f01f344c3b2ddcac866322b
Juan Pablo Ramirez Navarro <ramirezjuanpablo536@gmail.com>
2024-11-26 22:12:46 -0600

    Se agrega la interfaz de los modulos

--- src/main/java/org/utleon/elzarape/ClienteController.java
+++ src/main/java/org/utleon/elzarape/ClienteController.java
@@ -1,4 +1,106 @@
 package org.utleon.elzarape;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
 public class ClienteController {
+
+    @FXML
+    private Button btnGuardar;
+
+    @FXML
+    private Button btnLimpiar;
+
+
+    @FXML
+    private VBox btnInicio;
+
+    @FXML
+    private TableColumn<?, ?> colApellidos;
+
+    @FXML
+    private TableColumn<?, ?> colCiudad;
+
+    @FXML
+    private TableColumn<?, ?> colContrasenia;
+
+    @FXML
+    private TableColumn<?, ?> colEstado;
+
+    @FXML
+    private TableColumn<?, ?> colEstatus;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colNombre;
+
+    @FXML
+    private TableColumn<?, ?> colTelefono;
+
+    @FXML
+    private TableColumn<?, ?> colUsuario;
+
+    @FXML
+    private TableView<?> tblClientes;
+
+    @FXML
+    private TextField txtApellidos;
+
+    @FXML
+    private ComboBox<?> txtCiudad;
+
+    @FXML
+    private TextField txtContrasenia;
+
+    @FXML
+    private ComboBox<?> txtEstado;
+
+    @FXML
+    private CheckBox txtEstatus;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtTelefono;
+
+    @FXML
+    private TextField txtUsuario;
+
+    public void initialize() {
+        btnInicio.setOnMouseClicked(event -> {
+            try {
+                cargarModuloPrincipal();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public void cargarModuloPrincipal() throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(LoginController.class.getResource("inicio.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("El Zarape - Sistema de Gestión");
+        stage.setMaximized(true);
+        stage.show();
+        stage = (Stage) btnGuardar.getScene().getWindow();
+        stage.close();
+    }
+
 }
